0921287159a169900fd94ef666f6b6d2a8533d38
Anish Neupane <anishneupane1@gmail.com>
2026-05-25 22:44:13 -0700

    Errors

---
+++ internal/errs/errs.go
@@ -0,0 +1,21 @@
+package errs
+
+import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	ErrInvalidCredentials    = status.Error(codes.Unauthenticated, "Invalid credentials")
+	ErrInvalidTokenOrExpired = status.Error(codes.Unauthenticated, "Invalid token or expired")
+	ErrNotFound              = status.Error(codes.NotFound, "Not found")
+	ErrSessionExpired        = status.Error(codes.Aborted, "Session expired")
+	ErrInvalidCode           = status.Error(codes.InvalidArgument, "Invalid code")
+	ErrPasswordMissMatch     = status.Error(codes.InvalidArgument, "Password miss match")
+	ErrPreviousPassword      = status.Error(codes.AlreadyExists, "Cannot use previously used password")
+	ErrAccountRestricted     = status.Error(codes.PermissionDenied, "Account restricted")
+	ErrAccountPending        = status.Error(codes.PermissionDenied, "Account not verified")
+	ErrInternalServer        = status.Error(codes.Internal, "Internal Server Error")
+	ErrRequestTimeout        = status.Error(codes.DeadlineExceeded, "Request timeout exceeded")
+	ErrCanceled              = status.Error(codes.Canceled, "Request canceled by client")
+)
